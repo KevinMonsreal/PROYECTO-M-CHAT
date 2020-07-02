b5d43071826b6aa5e9f31ecb24304537b2583280
kevin Monsreal <kevinmonsreal99@gmail.com>
2020-07-02 10:40:13 -0500

    proyecto M-CHAT actualizado
    DESCRIPCION:
    este proyecto nos ayuda a detectar posibles casos de autismo en los niños

---
+++ M-CHAT/M-CHAT/M-CHAT.Services/Migrations/20200702144631_inicial.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace M_CHAT.Services.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Centros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAT = table.Column<DateTime>(nullable: false),
+                    UpdatedAT = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    Nombre_Centro = table.Column<string>(nullable: false),
+                    Direccion = table.Column<string>(nullable: false),
+                    Clave = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Centros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAT = table.Column<DateTime>(nullable: false),
+                    UpdatedAT = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    Nombre = table.Column<string>(nullable: false),
+                    Direccion = table.Column<string>(nullable: true),
+                    Correo = table.Column<string>(nullable: false),
+                    Contra = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Niños",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAT = table.Column<DateTime>(nullable: false),
+                    UpdatedAT = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    Nombre_Completo = table.Column<string>(nullable: false),
+                    CURP = table.Column<string>(nullable: false),
+                    Fecha_Nacimiento = table.Column<DateTime>(nullable: false),
+                    Edad = table.Column<int>(nullable: false),
+                    Genero = table.Column<string>(nullable: false),
+                    TutorID = table.Column<int>(nullable: false),
+                    CentroID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Niños", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Niños_Centros_CentroID",
+                        column: x => x.CentroID,
+                        principalTable: "Centros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Niños_Turores_TutorID",
+                        column: x => x.TutorID,
+                        principalTable: "Turores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Preguntas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAT = table.Column<DateTime>(nullable: false),
+                    UpdatedAT = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    Texto = table.Column<string>(nullable: false),
+                    Valor = table.Column<string>(nullable: true),
+                    NiñoID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Preguntas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Preguntas_Niños_NiñoID",
+                        column: x => x.NiñoID,
+                        principalTable: "Niños",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Niños_CentroID",
+                table: "Niños",
+                column: "CentroID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Niños_TutorID",
+                table: "Niños",
+                column: "TutorID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Preguntas_NiñoID",
+                table: "Preguntas",
+                column: "NiñoID",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Preguntas");
+
+            migrationBuilder.DropTable(
+                name: "Niños");
+
+            migrationBuilder.DropTable(
+                name: "Centros");
+
+            migrationBuilder.DropTable(
+                name: "Turores");
+        }
+    }
+}
